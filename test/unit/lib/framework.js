69a76c1b8ca433507df35b6fbf74d7a7a8fe6869
mdandrea <mdandrea@rim.com>
2012-08-27 11:46:26 -0400

    Added support to allow JavaScript and networking to work while an app is running in the background.

--- test/unit/lib/framework.js
+++ test/unit/lib/framework.js
@@ -40,6 +40,7 @@ describe("framework", function () {
             active: undefined,
             zOrder: undefined,
             url: undefined,
+            autoDeferNetworkingAndJavaScript: undefined,
             setGeometry: jasmine.createSpy(),
             onNetworkResourceRequested: undefined,
             destroy: jasmine.createSpy(),
@@ -76,7 +77,7 @@ describe("framework", function () {
         controllerWebView = require(srcPath + "controllerWebView");
         rotationHelper = require(srcPath + 'rotationHelper');
         spyOn(rotationHelper, "addWebview");
-        spyOn(webview, "create").andCallFake(function (done) {
+        spyOn(webview, "create").andCallFake(function (config, done) {
             done();
         });
         spyOn(overlayWebView, "create").andCallFake(function (done) {
@@ -106,12 +107,7 @@ describe("framework", function () {
 
     it("on start passing callback and setting object parameters to create method of webview", function () {
         framework.start();
-        expect(webview.create).toHaveBeenCalledWith(jasmine.any(Function), jasmine.any(Object));
-    });
-
-    it("setting object should have debugEnabled to be defined", function () {
-        framework.start();
-        expect((webview.create.mostRecentCall.args)[1].debugEnabled).toBeDefined();
+        expect(webview.create).toHaveBeenCalledWith(jasmine.any(Object), jasmine.any(Function));
     });
 
     it("can start a webview instance with a url", function () {
