aa201de5a67731e4aef74c5da15dd73e071e9bc5
Nukul Bhasin <me@nukulb.com>
2012-10-25 08:23:51 -0400

    Removes unknown protocol and adds handling for InvokeRequest event.
    Fixes blackberry/BB10-WebWorks-Framework#323
    Automated tests are availabe under
    test/functional/automatic/invokeRequestEvent.js

--- test/unit/lib/framework.js
+++ test/unit/lib/framework.js
@@ -45,7 +45,8 @@ describe("framework", function () {
             destroy: jasmine.createSpy(),
             executeJavaScript: jasmine.createSpy(),
             windowGroup: undefined,
-            addEventListener: jasmine.createSpy()
+            addEventListener: jasmine.createSpy(),
+            uiWebView: undefined
         };
         mockedApplicationWindow = {
             visible: undefined
@@ -87,6 +88,7 @@ describe("framework", function () {
         spyOn(webview, "destroy");
         spyOn(webview, "executeJavascript");
         spyOn(webview, "setURL");
+        spyOn(webview, "setUIWebViewObj");
         spyOn(overlayWebView, "setURL");
         spyOn(overlayWebView, "renderContextMenuFor");
         spyOn(overlayWebView, "handleDialogFor");
